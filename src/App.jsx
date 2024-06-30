e195bf1912b26a4a70ddfa01cff0404a7af98857
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-30 14:55:13 +0100

    Added route paths, home rendering Layout component  and admin rendering AdminLayout and displaying  AdminDashboard via outlet at at same route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./components/Layout";
+import AdminLayout from "./components/AdminLayout";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {index: true, element: <h1>Testing now!</h1>},
+        {path: 'admin', element: <AdminLayout />,
+          children: [
+            {index: true, element: <AdminDashboard />}
+          ]
+        }
+      ]
+    }
+  ])
+
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
