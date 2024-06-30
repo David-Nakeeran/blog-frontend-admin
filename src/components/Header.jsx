8c37f9f775e3f0311048deea3f54b4c29b451f33
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-30 13:56:10 +0100

    Header component, with NavLinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
+import { NavLink } from "react-router-dom";
 
+
+const activeLinkStyle = (isActive) => {
+    return isActive ? "active-link" : null;
+}
 
 export default function Header() {
 
     return (
-        <>
-        </>
+        <header>
+            <nav>
+                <div>
+                    <NavLink
+                        to="."
+                        className={({isActive}) => 
+                        activeLinkStyle(isActive)}>
+                        Home
+                    </NavLink>
+                </div>
+                <div>
+                    <NavLink
+                        to="/login"
+                        className={({isActive}) => 
+                        activeLinkStyle(isActive)}>
+                        Login
+                    </NavLink>
+                </div>
+            </nav>
+        </header>
     )
 }
