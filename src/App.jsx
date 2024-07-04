dfb4bdbf2f48cbbbf2f866a7d73dfc719a6d3a61
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-07-04 13:53:11 +0100

    Added child routes for admin path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Layout from "./components/Layout";
+import Error from "./components/Error";
+import Login from "./pages/Login";
+import Home, {loader as homeLoader} from "./pages/Home";
+import PostDetail, {loader as postDetailLoader} from "./pages/PostDetail";
+import PostList, {loader as postListLoader} from "./pages/PostList";
+import PostEdit, {loader as postEditLoader} from "./pages/PostEdit";
 import AdminLayout from "./components/AdminLayout";
 import AdminDashboard from "./pages/AdminDashboard";
 
@@ -13,10 +19,20 @@ function App() {
       path: "/",
       element: <Layout />,
       children: [
-        {index: true, element: <h1>Testing now!</h1>},
+        {
+          index: true,
+          element: <Home />,
+          errorElement: <Error />, 
+          loader: homeLoader
+        },
+        {path: 'login', element: <Login />},
         {path: 'admin', element: <AdminLayout />,
           children: [
-            {index: true, element: <AdminDashboard />}
+            {index: true, element: <AdminDashboard />},
+            {index: true, element: <PostList />, loader: postListLoader},
+            {index: true, element: <PostDetail />, loader: postDetailLoader},
+            {index: true, element: <PostEdit />, loader: postEditLoader}
+
           ]
         }
       ]
