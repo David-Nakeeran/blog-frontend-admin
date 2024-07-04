beb01c80d030c9f4dd16c6a5c7ff1e39451c8adf
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-07-04 13:56:43 +0100

    Implemented two functions  - handleChange keeps state and form in sync  - handleSubmit POST fetch form data to server

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,16 +6,56 @@ export default function Login() {
         password: ""
     });
 
-    const id = useId();
+    const id = useId(); // Generates unique id for accessibility attributes
 
-    // handleChange function
+    // Keeps state and form in sync
+    function handleChange(e) {
+        const {name, value} = e.target;
+        return setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            };
+        });
+    };
 
     // handleSubmit function
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const serialiseBody = JSON.stringify(formData);
+            const fetchOptions = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: serialiseBody
+        }
+            const response = await fetch('http://localhost:3000/login', fetchOptions);
+
+            if(!response.ok) {
+                throw new Error(`HTTP error, status: ${response.status}`)
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+        }   catch(error) {
+            console.error('There was a problem with the fetch operation:', error)
+        }
+        
+    }
+    
+    // TODO
+    // 1. store token in localstorage
+    // 2. navigate to adminDashboard
+    // 3. After submit clear state
 
     return (
         <div className='login-container'>
             <h1>Admin sign in</h1>
-            <form>
+            <form onSubmit={handleSubmit} method='POST'>
                 <label htmlFor={`${id}-username`}>Your email</label>
                 <input 
                 type='email'
